3093c296b6076e661a38dc5ac5137b16c1513eb2
Srabosthy23e <srabosthycse@gmail.com>
2024-03-25 23:58:13 +0600

    BookDetails page design done with responsive

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -10,7 +10,28 @@ const BookDetails = () => {
 
     return (
         <div>
-            <h2>kdjskdjsk</h2>
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 mt-10 mb-10">
+                <div>
+                    <img className="h-[530px] shadow-lg p-20 bg-base-200" src={book.image} alt="" />
+                </div>
+                <div className="space-y-3 lg:col-span-2">
+                    <h2 className="text-3xl font-bold">{book.bookName}</h2>
+                    <p className="font-semibold">By: {book.author}</p>
+                    <p>{book.category}</p>
+                    <hr />
+                    <p><span className="font-bold">review:</span> {book.review}</p>
+                    <p><span className="font-bold">Tag: </span> <button className="btn text-green-500 font-bold rounded-full">#{book.tags}</button></p>
+                    <hr />
+                    <p><span>Number of Pages: </span>{book.totalPages}</p>
+                    <p><span>Publisher: </span>{book.publisher}</p>
+                    <p><span>Year Of Publishing: </span>{book.yearOfPublishing}</p>
+                    <p><span>Rating: </span>{book.rating}</p>
+                    <div className="flex gap-5">
+                        <button className="btn font-bold text-lg px-8">Read</button>
+                        <button className="btn font-bold text-lg text-white bg-sky-400">Wishlist</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
