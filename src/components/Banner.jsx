137bcffbf01de9544b1f740f3b1a73354ee937c0
Srabosthy23e <srabosthycse@gmail.com>
2024-03-25 20:42:35 +0600

    banner section added with responsive

---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+const Banner = () => {
+    return (
+        <div className="hero mt-5 bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src="/public/banner.png" className="max-w-sm rounded-lg " />
+                <div>
+                    <h1 className="text-5xl font-bold lg:w-[500px]">Books to freshen up your bookshelf</h1>
+                    <button className=" mt-10 btn bg-green-500 text-white">View The List</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
