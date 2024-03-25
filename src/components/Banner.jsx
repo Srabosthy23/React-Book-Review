a04a03172654b707ddad3e3240702742fd392450
Srabosthy23e <srabosthycse@gmail.com>
2024-03-25 20:55:40 +0600

    after clicking view the list it goes to listed book task done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 const Banner = () => {
+
+    let navigate = useNavigate();
+    const handleViewList = () =>{
+        let path = "/listbook";
+        navigate(path);
+    }
+
     return (
         <div className="hero mt-5 bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img src="/public/banner.png" className="max-w-sm rounded-lg " />
                 <div>
                     <h1 className="text-5xl font-bold lg:w-[500px]">Books to freshen up your bookshelf</h1>
-                    <button className=" mt-10 btn bg-green-500 text-white">View The List</button>
+                    <button onClick={handleViewList} className=" mt-10 btn bg-green-500 text-white">View The List</button>
                 </div>
             </div>
         </div>
